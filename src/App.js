548af1373172b3f67e683fda253e788a6f8ffd8c
Justyna Zielkowska <justynazielkowska@gmail.com>
2023-06-06 11:53:45 +0200

    Added simple example of useState hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './App.css';
 import Task1 from './task-1/task-1';
@@ -7,14 +8,36 @@ import Task3 from './task-3/task-3';
 import Task4 from './task-4/task-4';
 import Task5 from './task-5/FilmLibrary';
 
+// prostu przyklad z uzyciem useState
 
-function App() {
+
+const StateExample  = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClicker = () => {
+    setIsVisible(!isVisible)
+  }
+
+  return (
+    <div>
+      <button onClick={handleClicker}>Click</button>
+      {isVisible ? <h2>Hello!</h2> : null}
+      
+    </div>
+  )
+
+}
+
+export default StateExample;
+
+
+const App = () => {
   return (
     <div className="App">
-      
-     <Task5/>
+
+    <StateExample/>
+    
     </div>
   );
 }
 
-export default App;
